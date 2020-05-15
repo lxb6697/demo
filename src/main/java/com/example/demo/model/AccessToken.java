c6f46b8f331b81b398c5bb26d51799354ffc37cd
liuxiaobin <liuxiaobin@yuewei007.com>
2020-05-15 22:43:02 +0800

    token示例代码提交

---
+++ src/main/java/com/example/demo/model/AccessToken.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class AccessToken {
+
+    /** token */
+    private String token;
+
+    /** 失效时间 */
+    private Date expireTime;
+
+}
