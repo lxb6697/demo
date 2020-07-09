27392b5b580c3b7cb3fe378ce0588a2dae4d52fe
liuxiaobin <liuxiaobin@yuewei007.com>
2020-07-09 16:16:20 +0800

    新增xml转json工具

---
+++ src/main/java/com/example/demo/utils/XmlToJsonUtil.java
@@ -0,0 +1,70 @@
+package com.example.demo.utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.dom4j.*;
+
+import java.util.List;
+
+/**
+ * @author liuxiaobin
+ * @date 2020/7/7 17:41
+ */
+public class XmlToJsonUtil {
+
+    public static void main(String[] args) throws DocumentException {
+        String xmlStr = "<itemRule><field id=\"prop_13021751\" name=\"款号\" type=\"input\"><rules><rule name=\"requiredRule\" value=\"true\"/></rules></field><field id=\"prop_20000\" name=\"品牌\" type=\"singleCheck\"><rules><rule name=\"requiredRule\" value=\"true\"/></rules><options><option displayName=\"南极人\" value=\"107380\"/></options></field></itemRule>";
+        JSONObject jsonObject = documentToJSONObject(xmlStr);
+        System.out.println(jsonObject);
+    }
+
+
+    /**
+     * String 转 org.dom4j.Document
+     * @param xml
+     * @return
+     * @throws DocumentException
+     */
+    public static Document strToDocument(String xml) throws DocumentException {
+        return DocumentHelper.parseText(xml);
+    }
+
+    /**
+     * org.dom4j.Document 转  com.alibaba.fastjson.JSONObject
+     * @param xml
+     * @return
+     * @throws DocumentException
+     */
+    public static JSONObject documentToJSONObject(String xml) throws DocumentException {
+        return elementToJSONObject(strToDocument(xml).getRootElement());
+    }
+
+    /**
+     * org.dom4j.Element 转  com.alibaba.fastjson.JSONObject
+     * @param node
+     * @return
+     */
+    public static JSONObject elementToJSONObject(Element node) {
+        JSONObject result = new JSONObject();
+        // 当前节点的名称、文本内容和属性
+        List<Attribute> listAttr = node.attributes();// 当前节点的所有属性的list
+        for (Attribute attr : listAttr) {// 遍历当前节点的所有属性
+            result.put(attr.getName(), attr.getValue());
+        }
+        // 递归遍历当前节点所有的子节点
+        List<Element> listElement = node.elements();// 所有一级子节点的list
+        if (!listElement.isEmpty()) {
+            for (Element e : listElement) {// 遍历所有一级子节点
+                if (e.attributes().isEmpty() && e.elements().isEmpty()) // 判断一级节点是否有属性和子节点
+                    result.put(e.getName(), e.getTextTrim());// 沒有则将当前节点作为上级节点的属性对待
+                else {
+                    if (!result.containsKey(e.getName())) // 判断父节点是否存在该一级节点名称的属性
+                        result.put(e.getName(), new JSONArray());// 没有则创建
+                    ((JSONArray) result.get(e.getName())).add(elementToJSONObject(e));// 将该一级节点放入该节点名称的属性对应的值中
+                }
+            }
+        }
+        return result;
+    }
+
+}
