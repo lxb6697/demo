72b11aeb47e412168e68c681c8757808d9a91a59
liuxiaobin <liuxiaobin@yuewei007.com>
2020-08-31 11:46:10 +0800

    拼多多key变更

--- src/main/java/com/example/demo/pdd/PopClientBuilder.java
+++ src/main/java/com/example/demo/pdd/PopClientBuilder.java
@@ -6,7 +6,7 @@ import com.pdd.pop.sdk.http.PopHttpClient;
 public class PopClientBuilder {
 
     private static final String clientId = "1ff407086074496cb5f3be18ac10df1f";
-    private static final String clientSecret = "75cef2f5c01a8041c76bee39af566e299ad748ad";
+    private static final String clientSecret = "33373f673897154d738798253e29fa115db0c27c";
     public static final String accessToken = "7c7a10b29dc6445ba462a090e202545e63dd0afb";
 
     public static PopClient builder() {
